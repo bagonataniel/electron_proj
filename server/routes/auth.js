52a19c45993067839ceec88de58a6028058a090e
bagonataniel <bagonataniel@gmail.com>
2024-11-27 14:03:36 +0100

    Reworking login and registration backend

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
+const mysql = require('mysql2')
 require('dotenv').config();
 const SECRET_KEY = process.env.SECRET_KEY;
+
+var pool = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,  // Number of connections in the pool
+  queueLimit: 0
+});
 
 function generateToken(user) {
     return jwt.sign({ id: user.id, username: user.username }, SECRET_KEY, { expiresIn: '1h' });
@@ -12,13 +23,24 @@ router.post('/login', (req, res) =>{
     const data = req.body;
     console.log(data)
 
-    if (data.username == "admin" && data.password == "1234" || data.username == "feri" && data.password == "12") {
-    const token = generateToken({ id: 1, username: data.username });
-    res.json({ token });
-    }
-    else{
-    res.status(401).json({ error: 'Invalid credentials' });
-    }
+    pool.execute("SELECT * FROM users WHERE email = ?", [data.username], function(err, result) {
+        if (err) {
+          console.error(err)
+          return res.status(500).json({ error: 'Database error' });
+        }
+
+        if (result.length === 0) {
+          return res.status(401).json({ error: 'Invalid credentials' });
+        }
+
+        if (data.password == result[0].password_hash) {
+          const token = generateToken({ id: result[0].user_id, username: data.username });
+          res.json({ token });
+        }
+        else{
+          res.status(401).json({ error: 'Invalid credentials' });
+        }
+    })
 })
 
 
--- server/server.js
+++ server/server.js
@@ -2,20 +2,28 @@ const express = require('express');
 const path = require('path');
 const jwt = require('jsonwebtoken')
 const authRoutes = require('./routes/auth')
-const mysql = require('mysql')
+const mysql = require('mysql2')
 require('dotenv').config();
 
 const app = express();
 const dataFilePath = path.join(__dirname, 'data.json');
 const SECRET_KEY = process.env.SECRET_KEY;
 
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "financial_manager"
-});
+// SECRET_KEY = "b9a712fc7a6f3e509dc2ac789ac08ea8395767179b04502fbcb"
+// HOST = "localhost"
+// USER = "root"
+// PASSWORD = ""
+// DATABASE = "financial_manager"
 
+var pool = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,  // Number of connections in the pool
+  queueLimit: 0
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -27,18 +35,18 @@ app.post('/register', async (req, res) =>{
   const data = req.body;
   console.log(data)
 
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO users (first_name, last_name, email, password_hash) VALUES (?);";
-    var values = [data.first_name, data.last_name, data.username, data.password];
+  pool.execute("INSERT INTO users (first_name, last_name, email, password_hash) VALUES (?,?,?,?)", [data.first_name, data.last_name, data.username, data.password], function(err, result) {
+    if (err) {
+      console.error(err)
+      if (err.code == "ER_DUP_ENTRY") {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
 
-    con.query(sql, [values], function (err, result) {
-      if (err) throw err;
-      console.log("Number of records inserted: " + result.affectedRows);
-      res.json({ message: 'Register successful' });
-    });
-  });
+    console.log("Register successful");
+    
+    res.json({ message: 'Register successful' });
+  })
 })
 
 
